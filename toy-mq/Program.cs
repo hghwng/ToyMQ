2b76740a7281628900f5446d8f1d025cfbade951
Hugh Wang <hghwng@gmail.com>
2016-10-01 21:24:25 +0800

    Detailed logging in main function.

--- toy-mq/Program.cs
+++ toy-mq/Program.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using ToyMQ.MessageQueue;
 
 namespace ToyMQ {
     class MainClass {
-        public static void Main (string[] args) {
-            var server = AdapterFactory.CreateServer("tcp://localhost:23333");
+        public static void Main(string[] args) {
+            Console.WriteLine("Creating server");
+            var server = AdapterFactory.CreateServer("pipe://s_server:s_client");
+            Console.WriteLine("Waiting for client");
             var client = server.WaitForNewClient();
             var str = "Hello world";
+            Console.WriteLine("Sending");
             client.Send(Encoding.ASCII.GetBytes(str));
 
             byte[] recv = new byte[6];
+            Console.WriteLine("Receiving");
             client.Receive(recv);
             Console.WriteLine(Encoding.ASCII.GetString(recv));
         }
