b13868d6c8917dc61b99c0613ea2311a31e5c5d7
Hugh Wang <hghwng@gmail.com>
2016-10-02 14:40:19 +0800

    Add test for MessageQueue.

--- toy-mq/Program.cs
+++ toy-mq/Program.cs
@@ -2,22 +2,16 @@
 using System.IO;
 using System.Text;
 using ToyMQ.MessageQueue;
+using ToyMQ.Serializer;
 
 namespace ToyMQ {
     class MainClass {
         public static void Main(string[] args) {
-            Console.WriteLine("Creating server");
-            var server = AdapterFactory.CreateServer("pipe://s_server:s_client");
-            Console.WriteLine("Waiting for client");
-            var client = server.WaitForNewClient();
-            var str = "Hello world";
-            Console.WriteLine("Sending");
-            client.Send(Encoding.ASCII.GetBytes(str));
-
-            byte[] recv = new byte[6];
-            Console.WriteLine("Receiving");
-            client.Receive(recv);
-            Console.WriteLine(Encoding.ASCII.GetString(recv));
+            if (args[0] == "server") {
+                ToyMQ.Test.MessageQueue.RunServer();
+            } else {
+                ToyMQ.Test.MessageQueue.RunClient();
+            }
         }
     }
 }
---
+++ toy-mq/Test/MessageQueue.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using ToyMQ.MessageQueue;
+
+namespace ToyMQ.Test {
+    public class MessageQueue {
+        private static void PrintMessage(Message msg) {
+            foreach (var envelope in msg.Envelopes) {
+                Console.WriteLine("    " + Encoding.ASCII.GetString(envelope));
+            }
+        }
+
+        public static void RunClient() {
+            Console.WriteLine("Client is running");
+            var server = AdapterFactory.CreateClient("tcp://localhost:23333");
+            PrintMessage(Message.ReceiveFrom(server));
+            Console.WriteLine("Hello from server");
+
+            var msg = new Message();
+            msg.Envelopes.Add(Encoding.ASCII.GetBytes("TestClient"));
+            Console.WriteLine("Sending reply");
+            msg.SendTo(server);
+
+            Console.WriteLine("Receiving bye");
+            PrintMessage(Message.ReceiveFrom(server));
+        }
+
+        public static void RunServer() {
+            Console.WriteLine("Server is running");
+            var server = AdapterFactory.CreateServer("tcp://localhost:23333");
+
+            var msgHello = new Message();
+            msgHello.Envelopes.Add(Encoding.ASCII.GetBytes("From server:"));
+            msgHello.Envelopes.Add(Encoding.ASCII.GetBytes("What's your name?"));
+
+            var msgBye = new Message();
+            msgBye.Envelopes.Add(Encoding.ASCII.GetBytes("Goodbye:"));
+            msgBye.Envelopes.Add(null);
+
+            while (true) {
+                var client = server.WaitForNewClient();
+                Console.WriteLine("New client");
+                msgHello.SendTo(client);
+
+                var reply = Message.ReceiveFrom(client);
+                msgBye.Envelopes[1] = reply.Envelopes[0];
+                msgBye.SendTo(client);
+                Console.WriteLine("End of reply");
+            }
+        }
+    }
+}
